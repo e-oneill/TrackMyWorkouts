9de079e87dd517aeffd68ccc26e5978726c6c297
e-oneill <eoneill@live.co.uk>
2021-11-20 11:04:47 +0000

    Initial commit

---
+++ src/components/Home.js
@@ -0,0 +1,49 @@
+import React from "react"
+import Logout from "./users/Logout";
+
+import AppTabBar from "./standard-page-parts/AppTabBar";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams
+} from "react-router-dom";
+
+
+
+
+import Typography from '@mui/material/Typography';
+import Card from '@mui/material/Card';
+
+//This home page exists only to test that the user is being logged in correctly and user is available in props for use elsewhere in application.
+class Home extends React.Component
+{
+  constructor(props){
+    super(props)
+    this.state = {
+      user: this.props.user
+    }
+  }
+  render() {
+    return (
+      <Router>
+        
+      <div style={{ display:'grid', justifyContent:'center' }}>
+      {(this.state.user.name) ?
+        <Card sx={{width:320, maxWidth: "90vw", padding: 1, margin: 1, display:'grid', justifyContent:'center'}}>
+        <Typography variant="h3">
+        Hello {this.state.user.name}!
+        </Typography>
+        </Card>:
+        <p>You must be logged in to see this site.</p>
+        }
+
+      </div>
+      </Router>
+    )
+  }
+} 
+
+export default Home;
