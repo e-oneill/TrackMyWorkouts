d4f91731b3c0e7e3baa773743d643d24c55afbc7
e-oneill <eoneill@live.co.uk>
2021-11-20 14:45:55 +0000

    Create framework for workout Created folder and main component for the workout class

---
+++ src/components/workout/Workout.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+class Workout extends React.Component {
+  render() {
+    return(
+      <div>
+        This will be the workout screen.
+      </div>
+    )
+  }
+}
+
+export default Workout;
